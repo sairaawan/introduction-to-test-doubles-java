6aa2c1e3232c58e7b2367389f203a97de608cf72
sairaawan <syraawan@live.com>
2022-01-26 11:43:11 +0000

    Activity 4 added for peachtea

--- src/test/java/BubbleTeaRouletteServiceTest.java
+++ src/test/java/BubbleTeaRouletteServiceTest.java
@@ -23,4 +23,20 @@ public class BubbleTeaRouletteServiceTest {
         assertEquals(expectedResult, actualResult);
 
     }
+
+    @Test
+    public void shouldGenerateARandomBubbleTeaTypePeachIceTeaWhenGetRandomBubbleTeaTypeIsCalled() {
+
+        //Arrange
+        RandomStub randomStub = new RandomStub(BubbleTeaTypeEnum.PeachIceTea.ordinal());
+        BubbleTeaRouletteService bubbleTeaRouletteService = new BubbleTeaRouletteService(randomStub);
+
+        //Act
+        var actualResult = bubbleTeaRouletteService.getRandomBubbleTeaType();
+        var expectedResult = BubbleTeaTypeEnum.PeachIceTea;
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+
+    }
 }
